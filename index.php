16d88cd8f35f853c3c89b9b031f9591a508b4bfe
Felipe <felipeh.soares3@hotmail.com>
2019-04-15 16:13:31 -0300

    Adicionando arquivo index a pasta do projeto

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, user-scalable=no">
+	<meta charset="utf-8">
+	<title>Sonia</title>
+	<link rel="stylesheet" href="assets/css/framework/bootstrap.min.css">
+
+</head>
+<body>
+
+	<header id="header">
+		
+		<dir class="container-fluid">
+			<div class="row justify-content-center">
+				<div class="col-8 text-center">
+					<h5>Sonia Cardoso</h5>
+					<p>
+						Serviços de depilação profissional
+					</p>
+				</div>
+			</div>
+		</dir>
+
+	</header>
+
+	<script src="assets/js/framework/jquery-3.3.1.min.js" type="text/javascript"></script>
+	<script src="assets/js/framework/bootstrap.bundle.min.js" type="text/javascript"></script>
+</body>
+</html>
